c764869be519629c00efe3eef58b17b766a2452c
Ruthuparnnan <ruthuparnnan123@gmail.com>
2023-10-26 04:08:14 +0530

    made marquee responsive and consistent

--- src/pages/MarqueeImages.jsx
+++ src/pages/MarqueeImages.jsx
@@ -22,8 +22,57 @@ function MarqueeImages() {
 
           <div class="marquee-item">
             <img
+              src="https://image.tmdb.org/t/p/w500/gC3tW9a45RGOzzSh6wv91pFnmFr.jpg"
               alt=""
-              src="https://image.tmdb.org/t/p/w500/jcUXVtJ6s0NG0EaxllQCAUtqdr0.jpg"
+            />
+          </div>
+
+          <div class="marquee-item">
+            <img
+              src="https://image.tmdb.org/t/p/w500/ogyw5LTmL53dVxsppcy8Dlm30Fu.jpg"
+              alt=""
+            />
+          </div>
+
+          <div class="marquee-item">
+            <img
+              src="https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
+              alt=""
+            />
+          </div>
+
+          <div class="marquee-item">
+            <img
+              alt=""
+              src="https://image.tmdb.org/t/p/w500/xmC2A7qPEfiOkQC58fZh8srWQ5v.jpg"
+            />
+          </div>
+
+          <div class="marquee-item">
+            <img
+              src="https://image.tmdb.org/t/p/w500/h4VB6m0RwcicVEZvzftYZyKXs6K.jpg"
+              alt=""
+            />
+          </div>
+
+          <div class="marquee-item">
+            <img
+              alt=""
+              src="https://image.tmdb.org/t/p/w500/tZ55C7gPExwzvBLCsZMqFZMbB2I.jpg"
+            />
+          </div>
+
+          <div class="marquee-item">
+            <img
+              src="https://w0.peakpx.com/wallpaper/953/11/HD-wallpaper-avatar-2009-poster-fantasy-movie-actress-avatar-couple-actor-blue.jpg"
+              alt=""
+            />
+          </div>
+
+          <div class="marquee-item">
+            <img
+              src="https://image.tmdb.org/t/p/w500/mm2t5dd1QFxtX6X56Z9U5ucsIb1.jpg"
+              alt=""
             />
           </div>
 
@@ -64,57 +113,8 @@ function MarqueeImages() {
 
           <div class="marquee-item">
             <img
-              src="https://i.pinimg.com/originals/74/50/2e/74502e04d4892977ca6ef6ddc1265590.webp"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
               alt=""
               src="https://image.tmdb.org/t/p/w500/tZ55C7gPExwzvBLCsZMqFZMbB2I.jpg"
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://i.pinimg.com/originals/74/50/2e/74502e04d4892977ca6ef6ddc1265590.webp"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://i.pinimg.com/originals/74/50/2e/74502e04d4892977ca6ef6ddc1265590.webp"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://i.pinimg.com/originals/74/50/2e/74502e04d4892977ca6ef6ddc1265590.webp"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://i.pinimg.com/originals/74/50/2e/74502e04d4892977ca6ef6ddc1265590.webp"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://i.pinimg.com/originals/74/50/2e/74502e04d4892977ca6ef6ddc1265590.webp"
-              alt=""
-            />
-          </div>
-
-          <div class="marquee-item">
-            <img
-              src="https://i.pinimg.com/originals/74/50/2e/74502e04d4892977ca6ef6ddc1265590.webp"
-              alt=""
             />
           </div>
         </div>
